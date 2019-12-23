4999a847c8339a61ca06ef275dd7276821f0f9b7
Ben Hernandez <benjammin234@gmail.com>
2019-12-22 17:15:24 -0800

    rough outline of js. still need to run a test to see how nested the response is

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 'use strict';
 
+const searchURL = "https://developer.nps.gov/api/v1/parks"
 
-// https://developer.nps.gov/api/v1/parks
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const selectedStates = $('#js-selected-states').val();
+        const maxResults = $('#js-max-results').val();
+        getNationalParks(selectedStates, maxResults);
+    });
+}
+
+function getNationalParks() {
+    const params = {
+        key: apiKey,
+        q: MediaQueryList,
+        part: 'snippet',
+        maxResults
+    };
+
+    const queryString = 
+    const url = searchURL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        }
+        throw neww Error(response.statusText);
+    })
+    .then(respsonseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-err-msg').text(`The man behind the curtain spilled his coffee and something went wrong: ${err.message}`);
+    });
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();
+    
+}
+
+
+
+
+
+
+$(watchForm);
